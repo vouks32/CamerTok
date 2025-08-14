1413882b7406939f32ce131aabc654702c22980d
Vouks <jotsaange@gmail.com>
2025-08-14 15:19:27 +0100

    app

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   //AsyncStorage.clear()
+  
   return (
     <NavigationContainer>
       <AuthProvider>
